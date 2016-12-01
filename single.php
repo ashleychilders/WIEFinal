266b088ca82ef92eea3c6e0dfe31a50c2da66fc0
Ashley Childers <ashley.childers@gmail.com>
2016-11-30 21:54:25 -0500

    Update margins on pages

--- single.php
+++ single.php
@@ -1,16 +1,19 @@
 <?php get_header(); ?>
-
+  <div class="container">
+    <div class="single">
   <div class="row">
-      <div class="twelve columns">
-
+      <div class="four columns">
+        <div class="post-thumbnail">
+            <?php the_post_thumbnail('large'); ?></div>
+      </div>
 <!-- BEGIN PAGE PHP -->
+      <div class= "eight columns">
             <?php
                 if( have_posts() ) :
                     while( have_posts() ) :
                       the_post();
                       if ( has_post_thumbnail() ) { ?>
-                        <div class="post-thumbnail">
-                            <?php the_post_thumbnail('large'); ?></div>
+
                             <?php } ?>
                                   <h2><?php the_title(); ?></h2>
                                   <?php the_content();
@@ -19,5 +22,7 @@
              <!-- End Loop -->
       </div>
     </div>
+  </div>
+  </div>
 <!-- End Section Container -->
 <?php get_footer(); ?>
