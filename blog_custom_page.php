c1e023cb292b7bc6bc20b1d8fe18a24e654d94c1
Ashley Childers <ashley.childers@gmail.com>
2016-12-03 17:12:16 -0500

    Update to blog page styling

--- blog_custom_page.php
+++ blog_custom_page.php
@@ -12,12 +12,13 @@ get_header(); ?>
   <div class="container">
     <div class="blogpage">
       <div class="row">
-          <div class="twelve columns">
+          <div class="eight columns">
+            <h1>Slick's Blog Posts</h1>
               <?php if (have_posts()) :
                   while (have_posts()) : the_post(); ?>
 
                         <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                         <?php the_excerpt(__('Continue reading »','example'));
                     endwhile; ?>
 
@@ -29,6 +30,9 @@ get_header(); ?>
                         <p><?php _e('None found.','example'); ?></p>
                     </div>
                 <?php endif; wp_reset_query(); ?>
+          </div>
+          <div class ="four columns">
+              <?php get_sidebar(); ?>
           </div>
         </div>
     </div>
